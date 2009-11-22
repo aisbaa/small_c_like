f3999c8654629f2f0aed876646275d6d000c1942
Paulius Pilkauskas <talonas@gmail.com>
2009-11-22 11:00:34 +0200

    created matrix

--- syntaxmatrix.h
+++ syntaxmatrix.h
@@ -2,40 +2,69 @@
 #define SMALL_C_LIKE_SYNTAX_MATRIX
 
 #include <fstream>
+#include <map>
 
 #include "textstream.h"
+#include "innerLang.h"
 
 using namespace std;
 
 const string defaultDelimeter = "::=";
-//const string defaultEndOfRule = "#";
+const string defaultEndOfRule = "#";
+
+typedef map<string, int> customMap;
 
 class SyntaxMatrix {
 
  private:
     ifstream   * file;
     TextStream * stream;
+    InnerLang  * lang;
 
-    string *  leftSides;
-    string ** rightSides;
+    int ** matrix;
+
+    customMap TerminalMap;
+    customMap RowMap;
+
+    map<string, int>::iterator it;
 
     const string * delimeter;
     const string * endOfRule;
 
+    void parseAll();
+    void buildMatrix();
+    
+    /* These methods parse left and right sides of grammar */
+    /* TODO:
+     *   - rewrite more intelligent parser
+     */
     string buildRule();
-
     string buildLeft();
 
- public:
-    SyntaxMatrix::SyntaxMatrix(string);
+    /* Tells if we have this term in our matrix */
+    bool isDefinedTerminal(string);
+    /* Tells if it is inner lang value */
+    bool isInnerLangValue(string);
 
-    void parseAll();
+    /* Add terminal to our matrix */
+    void addTerminal(string);
+
+    void printTerminals();
+    void printRows();
+    void printMatrix();
+
+ public:
+    SyntaxMatrix::SyntaxMatrix(string, InnerLang *);
 
     /*
      * GETTERS
      */
 
-    int getActionNumber(string);
+    int getNumberByRow(string);
+
+    int getNumberByColumn(string);
+
+    int ** getMatrix();
 
 };
 
